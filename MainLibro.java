288621a2d4fe68c45c65ea316072900d223140b5
Cesar Lechuga <cesaryael2004@hotmail.com>
2024-04-27 00:01:33 -0600

    version 1

---
+++ MainLibro.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MainLibro {
+    public static void main(String[] args) {
+        Libro librito = new Libro();
+        String a, e;
+        int i, o;
+
+        Scanner op = new Scanner(System.in);
+        System.out.print("Ingresa el Titulo del libro: ");
+        a = op.next();
+        System.out.print("Ingresa el nombre del Autor: ");
+        e = op.next();
+        System.out.print("Ingresa el ISBN: ");
+        i = op.nextInt();
+        System.out.print("Ingresa El numero de paginas: ");
+        o = op.nextInt();
+
+        librito.titulo = a;
+        librito.autor = e;
+        librito.isbn = i;
+        librito.numpag = o;
+
+        librito.MostrarTitulo();
+        librito.MostarAutor();
+        librito.MostrarISBN();
+        librito.MostarNumeroDePaginas();
+        Libro libro1 = new Libro("Los bros", "Lechuga", 13, 100);
+
+    }
